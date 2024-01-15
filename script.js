5df62fd1f178c990c4831d713f202d3d8ca0fbbf
Mateus Nascimento <124949062+MateusJn@users.noreply.github.com>
2024-01-14 22:02:10 -0300

    Envio do Arquivo JS

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const key = "a371571b9ab807ac095e627cfa70af21"
+
+function adicionarDados(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    adicionarDados(dados)
+}
+
+function clickbotao () {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
